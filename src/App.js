b1745526ab68e37edb6193ab1a89f30b904ff19b
Bukhari <zainab.bukhari@afiniti.com>
2024-02-07 15:56:47 +0500

    Created app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useEffect, useState } from "react";
 function App() {
+  const [time, setTime] = useState({ hour: 0, minute: 0, seconds: 0 });
+
+  useEffect(() => {
+    const currentTime = new Date();
+    setTime({
+      hour: currentTime.getUTCHours(),
+      minute: currentTime.getUTCMinutes(),
+      seconds: currentTime.getUTCSeconds(),
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: "flex", justifyContent: "center" }}>
+      <div className="clock"></div>
     </div>
   );
 }
